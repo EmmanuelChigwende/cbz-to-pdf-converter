65fa42c1a3613fa0ae36afe23844a0d05f004d92
Emmanuel Chigwende <emmanuelchigwende2@gmail.com>
2026-01-30 22:51:57 +0200

    redesinged frontend and logic

--- main/src/Pages/Home.jsx
+++ main/src/Pages/Home.jsx
@@ -1,60 +1,65 @@
 import React from 'react'
 import { useState } from 'react'
-import {toast} from 'react-hot-toast'
+import toast from 'react-hot-toast'
 import JSZip from 'jszip'
 
 const Home = () => {
 
-  const {cbzFile,setCbzFile} = useState(null)
-  const form = new FormData()
+  const [cbz,setCbz] = useState(null)
 
+  function HandleUpload(e){
+    const file = e.target.files[0]
 
-  function HandleCbz(e) {
-    setCbzFile(cbzFile.files[0])
-    form.append("cbz",cbzFile)
+    if(!file){
+      toast.error("Invalid file maybe")
+    }
+    else{
+      setCbz(file)
+      toast.success("file uploaded sucessfully")
+    }
   }
 
-  async function HandleCbzParsing(e) {
-    try{
-      const zip =  await JSZip.loadAsync(form)
-      if(zip){
-        toast.success("first step complete")
+  async function HandleFile(e) {
+    if(!cbz){
+      toast.error("Please Upload File First")
+    }
+    else{
+      try{  
+        const zipfile = await JSZip.loadAsync(cbz)
+        const imagefiles = Object.keys(zipfile.files).filter(name => name.matchAll())
+
+        if(imagefiles.length === 0){
+          toast.error("No Images In Uploaded Cbz File")
+        }
+        else{
+
+          let blob = new Blob(imagefiles)
+
+          toast.success(blob.size)
+          toast.success("Cbz File Parsed Successfully")
+        }
+      }
+      catch(err){
+        toast.error("Failed To Parse Cbz File")
       }
     }
-    catch(err){
-      toast.error(err)
-    }
   }
-
 
 
   return (
-    <div className='p-4 font-sarif flex justify-center items-center h-[100vh]'>
-      <div className='h-[500px] w-[500px] bg-white rounded-lg p-2'>
+    <div className='h-[100vh] grid justify-center items-center'>
+      <div className='h-[200px] w-[500px] p-2 bg-white rounded-lg'>
+        <h1 className='text-[1.5rem] font-bold mb-[20px]'>
+          Convert cbz to pdf
+        </h1>
         <div className='grid gap-3'>
-          <h1 className='font-bold text-[2rem]'>
-          Cbz To Pdf Converter 
-          </h1>
-          <p className='text-[0.8rem] font-bold'>
-            convert your cbz files to pdf and read them anywhere anytime.....
-          </p>
-        </div>
-        <div className='mt-[30px]'>
-          <h2>
-            Please upload cbz file😊
-          </h2>
-          <div className='mt-[20px]' onChange={HandleCbz}>
-            <input type="file" accept='.cbz' name="" id="" />
-          </div>
-          <div className='mt-[10px]'>
-            <button onClick={HandleCbzParsing} className='bg-green-500 text-[1.5rem] p-1 rounded-lg text-white'>
-              convert
-            </button>
-          </div>
+          <input className='text-[1.5rem]' onChange={HandleUpload} accept='.cbz' type="file" name="" id="" />
+          <button onClick={HandleFile} className='bg-green-500 text-white text-[1.5rem] rounded-md p-1'>Convert</button>
         </div>
       </div>
     </div>
-  )
+
+)
 }
 
 export default Home
