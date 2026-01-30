e7faebe86666f224b67744aeee24d22d70818766
Emmanuel Chigwende <emmanuelchigwende2@gmail.com>
2026-01-30 11:01:48 +0200

    ui elements

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-red-200 h-[100vh]'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </div>
   )
 }
---
+++ main/src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='p-4 font-sarif flex justify-center items-center'>
+      <div className='h-[500px] w-[500px] bg-white rounded-lg p-2'>
+        <div className='grid gap-3'>
+          <h1 className='font-bold text-[2rem]'>
+          Cbz To Pdf Converter 
+          </h1>
+          <p className='text-[0.8rem] font-bold'>
+            convert your cbz files to pdf and read them anywhere anytime.....
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
